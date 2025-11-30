9af9c7accdb5de6116356d3a73676a7e5d1ae90b
smit <smitpatil453@gmail.com>
2025-11-30 14:17:58 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -108,5 +108,9 @@ def api_project():
 
 
 if __name__ == "__main__":
-    # Runs the app on http://127.0.0.1:5000
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
